a2b3ea136fc359b925505e2a931eb948d225714f
santosh33 <santosh33@gmail.com>
2022-06-08 22:04:08 +0530

    Introduce helper class

--- src/App.js
+++ src/App.js
@@ -14,15 +14,26 @@ class App extends React.Component{
         (err) => this.setState({errorMessage:err.message})
       );
   }
+
+  renderContent(){
+      //conditional rendering
+      if(this.state.errorMessage && !this.state.lat){
+        return(<div>Error:{this.state.errorMessage}</div>);
+      }
+      else if(!this.state.errorMessage && this.state.lat){
+        return(<SeasonDisplay lat={this.state.lat}/>);
+      }
+      else{
+        return <Spinner message="Please accept location request" />;
+      }
+  }
   
   render(){
-    //conditional rendering
-    if(this.state.errorMessage && !this.state.lat)
-      return(<div>Error:{this.state.errorMessage}</div>);
-    else if(!this.state.errorMessage && this.state.lat)
-      return(<SeasonDisplay lat={this.state.lat}/>);
-    else
-      return <Spinner message="Please accept location request" />;
+    return(
+      <di className="border red">
+        {this.renderContent()}
+      </di>
+    );
   }
 }
 
