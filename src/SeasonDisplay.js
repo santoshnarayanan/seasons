1a1f7ccea610739a6575815546d9fdcbf6e22c7d
santosh33 <santosh33@gmail.com>
2022-06-08 21:07:34 +0530

    Adding style

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import "./SeasonDisplay.css";
 import React from 'react';
 
 const seasonConfig = {
@@ -20,11 +21,13 @@ const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
     const {text, iconName} = seasonConfig[season];
 
+    //refer semantic ui for how to display size of icon. for e.g. we used massive - means 
+    // huge icons on browser when shows up
     return( 
-        <div>
-            <i className={`${iconName} icon`} />
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
             <h1>{text}</h1>
-            <i className={`${iconName} icon`} />
+            <i className={`icon-right massive ${iconName} icon`} />
         </div>
     );
 };
